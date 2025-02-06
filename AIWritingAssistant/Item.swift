49304a5cce20422fe912364ef67ac98cef8c738c
kenny <kenny.xue@michelin.com>
2025-02-06 10:06:38 +0800

    Initial Commit

---
+++ AIWritingAssistant/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AIWritingAssistant
+//
+//  Created by 薛毅华 on 2025/2/6.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AIWritingAssistantTests/AIWritingAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIWritingAssistantTests.swift
+//  AIWritingAssistantTests
+//
+//  Created by 薛毅华 on 2025/2/6.
+//
+
+import Testing
+@testable import AIWritingAssistant
+
+struct AIWritingAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
